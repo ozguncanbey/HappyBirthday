12ac602fb0a66b2700315be05aee5c1b571e7e3a
Özgün Can Beydili <ozguncanbeydili@hotmail.com>
2024-07-04 15:46:00 +0300

    Initial Commit

---
+++ HappyBirthday/HappyBirthdayApp.swift
@@ -0,0 +1,17 @@
+//
+//  HappyBirthdayApp.swift
+//  HappyBirthday
+//
+//  Created by Özgün Can Beydili on 4.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct HappyBirthdayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
